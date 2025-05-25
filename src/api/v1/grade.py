b869f7c45be432e942daef67f9a54e003efc895b
MonkaS <51020901+imMonkaS@users.noreply.github.com>
2025-05-25 22:06:10 +0300

    ISI-4 Настройки прав доступа преподавателя (#7)

--- src/api/v1/grade.py
+++ src/api/v1/grade.py
@@ -2,25 +2,47 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
+from dependencies.discipline import DisciplineServiceDependency
 from dependencies.grade import GradeServiceDependency
+from dependencies.professor import ProfessorServiceDependency
+from dependencies.user import VerifyTokenDependency
+from modules.Disciplines.schemas import SearchDisciplineSchema
 from modules.Grades.schemas import GetGradeResponse, CreateGradeSchema, SearchGradeSchema, GetManyGradesResponse, \
     UpdateGradeSchema, DefaultGradeResponse, DeleteGradeSchema
+from modules.Professors.schemas import SearchProfessorSchema
+from utils.base_schema import DefaultResponse
 
 GRADE_ROUTER = APIRouter(
     prefix='/grade',
     tags=['grade']
 )
 
-@GRADE_ROUTER.post('/create', response_model=GetGradeResponse)
+@GRADE_ROUTER.post('/create')
 async def create_grade(
         request_data: CreateGradeSchema,
-        service: GradeServiceDependency
-) -> GetGradeResponse:
-    prof = await service.create(request_data)
-    return GetGradeResponse(
-        message='Grade created',
-        data=prof
-    )
+        service: GradeServiceDependency,
+        prof_service: ProfessorServiceDependency,
+        username: VerifyTokenDependency
+) -> GetGradeResponse | DefaultResponse:
+    try:
+        professor = await prof_service.get_one(SearchProfessorSchema(username=username))
+        print(professor.disciplines)
+        if request_data.discipline_id not in [item.id for item in professor.disciplines]:
+            return DefaultResponse(
+                message='You are not a professor of this discipline'
+            )
+
+        request_data.professor_id = professor.id
+        prof = await service.create(request_data)
+        return GetGradeResponse(
+            message='Grade created',
+            data=prof
+        )
+    except Exception:
+        return DefaultResponse(
+            message='Something went wrong'
+        )
+
 
 
 @GRADE_ROUTER.get('/getOne', response_model=GetGradeResponse)
